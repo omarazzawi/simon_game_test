a166a6b777b35ac80ff14bab341e1564cfd0f563
omarazzawi <omar.alazzawi1987@gmail.com>
2025-09-19 23:29:25 +0200

    Add tests for newGame function

--- scripts/game.js
+++ scripts/game.js
@@ -2,7 +2,18 @@ let game =  {
     score: 0,
     currentGame:[],
     playMoves:[],
-    choices:[],
+    choices:["button1","button2", "button3","button4"],
 }
 
-module.exports = { game };
+function newGame() {
+    game.score = 0;
+    game.playMoves = [];
+    game.currentGame = [];
+    showScore();
+}
+
+function showScore() {
+    document.getElementById("score").innerText = game.score;
+}
+module.exports = { game, newGame , showScore };
+
--- scripts/tests/game.test.js
+++ scripts/tests/game.test.js
@@ -3,7 +3,7 @@
  */
 
 
-const { game } = require("../game");
+const { game, newGame, showScore } = require("../game");
 
 
 beforeAll(() => {
@@ -17,11 +17,39 @@ describe("game object contains correct keys", () => {
         expect("score" in game).toBe(true);
     });
     test("playMoves key exists", () => {
-        expect("playMoves" in game).toBe(true)
+        expect("playMoves" in game).toBe(true);
     });
 
     test("choices key exists", () => {
-        expect("choices" in game).toBe(true)
+        expect("choices" in game).toBe(true);
     });
 
+    test("choices contain correct ids", () => {
+        expect(game.choices).toEqual(["button1", "button2", "button3","button4"]);
+    });
+
+});
+
+describe("newGame works correctly", () => {
+    beforeAll(() => {
+        game.score = 42;
+        game.playMoves = ["button1", "button2"];
+        game.currentGame = ["button1", "button2"];
+        document.getElementById("score").innerText = "42";
+        newGame();
+
+    });
+    test("should set score to zero", () => {
+        expect(game.score).toEqual(0); // Rest the score to zero
+    });
+    test("should clear the player moves array", () => {
+        expect(game.playMoves).toEqual([]); // Clear the playerMoves
+    });
+    test(" should clear the current game array", () => {
+        expect(game.currentGame).toEqual([]); // Clear the currentGame array
+        //Could be writen like; expect(game.currentGame.length).toBe(0);
+    });
+    test("should display 0 for the elment with id of score", () => {
+        expect(document.getElementById("score").innerText).toEqual(0);
+    });
 });
